fb303651a0e77e4a660de32cee8095390147e7e4
CamiloRodriguez11 <camilo.rodriguez11@usa.edu.co>
2025-02-17 07:19:44 -0500

    Update ex1.py

---
+++ ex1.py
@@ -0,0 +1,8 @@
+a = int(input("Ingrese el primer numero: "))
+b = int(input("Ingrese el segundo numero: "))
+
+print ("La suma es: ",a+b)
+print ("La resta es: ",a-b)
+print ("La multiplicacion es: ",a*b)
+print ("La division es: ",a/b)
+print ("El residuo de la division es: ",a%b )

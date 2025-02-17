657ab4ddb520956d3e070e3c97165920b6d36dfd
CamiloRodriguez11 <camilo.rodriguez11@usa.edu.co>
2025-02-17 07:20:43 -0500

    Update ex3.cc

---
+++ ex3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    cout << "La variable inicial es: " << a << endl;
+    a += 5;
+    cout << "Aumentado 5: " << a << endl;
+    a -= 3;
+    cout << "Reduciendo 3: " << a << endl;
+    a *= 2;
+    cout << "Multiplicado por 2: " << a << endl;
+    a /= 4;
+    cout << "Dividido en 4: " << a << endl;
+  return 0;
+}

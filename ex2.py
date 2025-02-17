8e259b37133a93aa0c6091fc6b8095a4ae562836
CamiloRodriguez11 <camilo.rodriguez11@usa.edu.co>
2025-02-17 07:20:32 -0500

    Update ex2.py

---
+++ ex2.py
@@ -0,0 +1,5 @@
+articulo = int(input("ingrese el precio de su articulo: "))
+descuento = int(articulo*0.15)
+total = int(articulo-descuento)
+print ("El descuento de su producto es: ", descuento)
+print("El valor total es: ", total)

f2001878dffe1a10254d38689b96d42436b8e991
CamiloRodriguez11 <camilo.rodriguez11@usa.edu.co>
2025-03-12 08:00:05 -0500

    Add files via upload

---
+++ abecedario.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout<< "Abecedario en minuscula: "<<endl;
+  for (char letra = 'a';letra <= 'z'; ++letra ){
+    cout<<letra<<"";}
+    cout<<endl;
+  cout << "Abecedario en mayuscula: " << endl;
+  for (char letra = 'A';letra<= 'Z';++letra) {
+      cout<<letra<<"";}
+      return 0;
+  }
---
+++ abecedario.py
@@ -0,0 +1,6 @@
+print ("Abecedario en minuscula: ")
+for letra in range (97,122):
+    print (chr(letra))
+print ("Abecedario en mayuscula")
+for letra in range (65,90):
+    print (chr(letra))
